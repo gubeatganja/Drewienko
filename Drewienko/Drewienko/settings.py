27208521a65aa76f29fe882bb15f791c53378648
Vladyslav <gubeatganja@gmail.com>
2020-11-05 00:46:25 +0100

    3 different applications: announcement, home, user

--- Drewienko/Drewienko/settings.py
+++ Drewienko/Drewienko/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
     'home',
+    'user',
+    'announcement',
 ]
 
 MIDDLEWARE = [
--- Drewienko/Drewienko/urls.py
+++ Drewienko/Drewienko/urls.py
@@ -20,8 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
     path('', include('home.urls')),
+    path('ads/', include('announcement.urls')),
+    path('user/', include('user.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
